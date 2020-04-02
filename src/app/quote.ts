d90c88bf3b2ea6f3876d1ff8969a786c96b16ec0
David <dgkilolo23@gmail.com>
2020-04-02 06:25:50 +0300

    adds upvote/downvote variables to the class.

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,16 +1,10 @@
 export class Quote {
-
   upVote: number;
   downVote: number;
-
   showDescription: boolean;
-
   constructor( public name: string, public description: string, public submittedBy: string, public time:Date ){
     this.showDescription = false;
     this.upVote = 0;
     this.downVote = 0;
   }
-
-  
-
 }
